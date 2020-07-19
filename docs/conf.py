db9ad68159f8f7f4336475899fd568b5d01b671b
albertwigmore <albert@albertwigmore.co.uk>
2020-07-19 18:42:51 +0100

    Add initial sphinx documentation

---
+++ docs/conf.py
@@ -0,0 +1,39 @@
+# -- Path setup --------------------------------------------------------------
+
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath('..'))  # Autodoc
+
+# -- Project information -----------------------------------------------------
+
+project = 'Mars Insight'
+copyright = '2020, Albert Wigmore'
+author = 'Albert Wigmore'
+release = '0.0.1'
+
+# # -- General configuration ---------------------------------------------------
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.coverage',
+    'sphinx.ext.napoleon',
+]
+
+templates_path = [
+]
+
+exclude_patterns = [
+]
+
+
+# # -- Options for HTML output -------------------------------------------------
+
+html_theme = 'press'
+
+# Theme options
+html_theme_options = {
+    "external_links": [
+        ("Github", "https://github.com/albertwigmore/mars-insight"),
+    ]
+}
---
+++ docs/src/index.rst
@@ -0,0 +1,5 @@
+Mars InSight API Client Documentation
+=====================================
+
+A python client to provide easy access to NASA's InSight: Mars Weather Service API. 
+
